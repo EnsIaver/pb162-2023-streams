65d2535e9befb443fc97ae947667554f4a0a0c08
Stanislav Zeman <zeman@standa.dev>
2023-04-10 14:45:57 +0200

    chore: project init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * @author Stanislav Zeman
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
